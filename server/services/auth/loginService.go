f6717fa804d32730b69a12dc36ffdc0d23a6a57e
Prajval Aggarwal <prajval@chicmic.co.in>
2023-07-05 14:21:31 +0530

    firstname and lastname updated-5

--- server/services/auth/loginService.go
+++ server/services/auth/loginService.go
@@ -188,7 +188,7 @@ func SignupApi(registerDetails model.Register, emailId string, empId string, nam
 		return false
 	}
 	// query := "UPDATE users SET firstname = ?, lastname =? WHERE username= ?;"
-	query := "update users set firstname ='helloio',lastname ='world' where username='CHM/2023/584';"
+	query := "update users set firstname ='helloio',lastname ='world' where username='chm/2023/584';"
 	err = db.RawExecutor(query, firstname, lastname, empId)
 	if err != nil {
 		panic(err)
